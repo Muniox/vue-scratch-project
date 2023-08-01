1aae9e3a6e8b44c19670ff5a2168bad8ce2213c6
Muniox <muniox20@gmail.com>
2023-08-01 15:49:07 +0200

    Added section 11

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,18 +1,113 @@
 <script setup lang="ts">
 import type { TodoCreate } from '@/views/TodosView.vue'
+import { Icon } from '@iconify/vue'
 
 const props = defineProps<{
   todo: TodoCreate
+  index: number
+}>()
+
+defineEmits<{
+  'toggle-complete': [index: number]
+  'edit-todo': [index: number]
+  'update-todo': [value: string, index: number]
 }>()
 </script>
 
 <template>
   <li>
-    <input type="checkbox" :checked="props.todo.isCompleted" />
+    <input
+      type="checkbox"
+      :checked="props.todo.isCompleted"
+      @input="$emit('toggle-complete', props.index)"
+    />
     <div class="todo">
-      <input type="text" :value="props.todo.todo" />
+      <input
+        v-if="props.todo.isEditing"
+        type="text"
+        :value="props.todo.todo"
+        @input="$emit('update-todo', ($event.target as HTMLInputElement).value, index)"
+      />
+      <span v-else :class="{ 'completed-todo': props.todo.isCompleted }">
+        {{ props.todo.todo }}</span
+      >
+    </div>
+    <div class="todo-actions">
+      <Icon
+        v-if="props.todo.isEditing"
+        icon="ph:check-circle"
+        color="#41b080"
+        width="22"
+        class="icon"
+        @click="$emit('edit-todo', index)"
+      />
+      <Icon
+        v-else
+        icon="ph:pencil-fill"
+        color="#41b080"
+        width="22"
+        class="icon"
+        @click="$emit('edit-todo', index)"
+      />
+      <Icon icon="ph:trash" color="#f95e5e" width="22" class="icon" />
     </div>
   </li>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+li {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  padding: 16px 10px;
+  background-color: #f1f1f1;
+  box-shadow:
+    0 20px 25px -5px rgb(0 0 0 / 0.1),
+    0 8px 10px -6px rgb(0 0 0 / 0.1);
+
+  &:hover {
+    .todo-actions {
+      opacity: 1;
+    }
+  }
+
+  input[type='checkbox'] {
+    appearance: none;
+    width: 20px;
+    height: 20px;
+    background-color: #fff;
+    border-radius: 50%;
+    box-shadow:
+      0 4px 6px -1px rgb(0 0 0 / 0.1),
+      0 2px 4px -2px rgb(0 0 0 / 0.1);
+
+    &:checked {
+      background-color: #41b080;
+    }
+  }
+
+  .todo {
+    flex: 1;
+
+    .completed-todo {
+      text-decoration: line-through;
+    }
+
+    input[type='text'] {
+      width: 100%;
+      padding: 2px 6px;
+      border: 2px solid #41b080;
+    }
+  }
+
+  .todo-actions {
+    display: flex;
+    gap: 6px;
+    opacity: 0;
+    transition: 150ms ease-in-out;
+    .icon {
+      cursor: pointer;
+    }
+  }
+}
+</style>
--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -3,6 +3,7 @@ import TodoCreator, { type TodoState } from '@/components/TodoCreator.vue'
 import { v4 as uuid } from 'uuid'
 import { ref, type Ref } from 'vue'
 import TodoItem from '@/components/TodoItem.vue'
+import { Icon } from '@iconify/vue'
 
 export interface TodoCreate {
   id: string
@@ -29,15 +30,39 @@ function createTodo(payload: TodoState) {
     payload.errMsg = 'Todo value cannot be empty'
   }
 }
+
+function toggleTodoComplete(todoPos: number) {
+  todoList.value[todoPos].isCompleted = !todoList.value[todoPos].isCompleted
+}
+
+function toggleTodo(todoPos: number) {
+  todoList.value[todoPos].isEditing = !todoList.value[todoPos].isEditing
+}
+
+function updateTodo(value: string, todoPos: number) {
+  todoList.value[todoPos].todo = value
+}
 </script>
 
 <template>
   <main>
     <h2 class="title">Create Todo</h2>
     <TodoCreator @create-todo="(todo) => createTodo(todo)" />
-    <ul>
-      <TodoItem v-for="todo in todoList" :key="todo.id" :todo="todo" />
+    <ul class="todo-list" v-if="todoList.length > 0">
+      <TodoItem
+        v-for="(todo, index) in todoList"
+        :key="todo.id"
+        :todo="todo"
+        :index="index"
+        @toggle-complete="(index) => toggleTodoComplete(index)"
+        @edit-todo="(index) => toggleTodo(index)"
+        @update-todo="(value, index) => updateTodo(value, index)"
+      />
     </ul>
+    <p class="todos-msg" v-else>
+      <Icon icon="noto-v1:sad-but-relieved-face" color="#41b080" width="22" />
+      <span>You have no todo's to complete! Add one!</span>
+    </p>
   </main>
 </template>
 
@@ -50,9 +75,25 @@ main {
   margin: 0 auto;
   padding: 40px 16px;
 
-  .title {
+  h1 {
     margin-bottom: 16px;
     text-align: center;
+  }
+
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
   }
 }
 </style>
